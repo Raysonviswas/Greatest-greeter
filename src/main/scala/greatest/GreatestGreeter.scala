72f651ce542167b5ad6f2b9270fd37e09259e219
Raysonviswas <rayson.viswas@digital.hmrc.gov.uk>
2018-09-24 17:06:37 +0100

    fixed

--- src/main/scala/greatest/GreatestGreeter.scala
+++ src/main/scala/greatest/GreatestGreeter.scala
@@ -1,9 +1,6 @@
 package greatest
 import greatest.models.{BankAccount, CashISAccount, Person, SavingsAccount}
-
 import scala.io.StdIn
-
-
 
 object Prompt {
   def ask(message: String): String = StdIn.readLine(message)
@@ -14,8 +11,7 @@ object GreatestGreeter extends App {
   val age = Prompt.ask("What is your age? ")
   val person = new Person(name, age.toInt)
   println(person.speak())*/
-
-  /*val name2 = Prompt.ask("What is your name? ")
+/*val name2 = Prompt.ask("What is your name? ")
   val age2 = Prompt.ask("What is your age? ")
   val cashISA = new CashISAccount("45676", 0.0)
   //creating new instance of cash isa called deposited with same account number
@@ -26,7 +22,6 @@ object GreatestGreeter extends App {
   val person2 = new Person(name2, age2.toInt, deposited)
 
   println(person2.speak())*/
-
   val normalAccount = new CashISAccount("l33t", 100)
   val loyalAccountDeposited = normalAccount.deposit(300.00)
   val loyal = new Person("Loyal customer", 22, loyalAccountDeposited)
